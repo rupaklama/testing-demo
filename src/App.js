edcd8d4766f36b3aed333b4fd5e10609c005ea34
Rupak <rupaklamapythondeveloper@gmail.com>
2020-10-31 10:39:50 -0700

    refactor dry with beforeEach func

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div data-test='component-app'>
-      <h1>Learn React Testing!</h1>
+    <div data-test="component-app">
+      <h1 data-test="counter-display">The counter is currently: </h1>
+      <button data-test="increment-button">Increment counter</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 // Enzyme configurations
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -10,23 +12,39 @@ Enzyme.configure({ adapter: new Adapter() });
 // In TDD testing, we always want Red Test before Green Test
 // meaning we want our test to fail before they pass
 
+// if there's a common code between multiple tests in a single file,
+// we use jest's beforeEach helper func to extract that common logic
+let wrapper; // let to reassigned new values to this variable several times during test
+beforeEach(() => {
+  // any logic we put here gets executed before all the tests below
+  wrapper = shallow(<App />);
+});
+
 // Test stages
 
 // shows button
-test('renders button', () => {});
+test('renders button', () => {
+  const button = wrapper.find('[data-test="increment-button"]');
+  expect(button.length).toEqual(1);
+});
 
 // displays counter
-test('renders counter display', () => {});
+test('renders counter display', () => {
+  const counterDisplay = wrapper.find('[data-test="counter-display"]');
+  expect(counterDisplay.length).toEqual(1);
+});
 
 // starting value
 test('counter starts at 0', () => {
-
+  const appComponent = wrapper.find('[data-test="component-app"]');
+  expect(appComponent.length).toEqual(1);
 });
 
 // click increments counter display
 test('clicking on button increments counter display', () => {
-
-})
+  const appComponent = wrapper.find('[data-test="component-app"]');
+  expect(appComponent.length).toEqual(1);
+});
 
 // test function
 // first arg - String description of the test, second arg - func with test logic
@@ -36,15 +54,15 @@ test('renders without error', () => {
   // wrapper - a shallow instance object we get from this is a wrapped version of App component
   // wrapper specifically means that this is a wrapped component that has some additional
   // functionalities loaded on top with Enzyme
-  const wrapper = shallow(<App />);
+  // const wrapper = shallow(<App />);
 
   // find method returns back an array which contains every elements/instances that matches
-  // the selector - .find(selector) 
+  // the selector - .find(selector)
   // although we only care about only one element
 
   // using data-test-attribute on the top level element of component - 'div' to test rendering
   // using expect statement to throw an error, attribute syntax ([href="foo"]
-  const appComponent = wrapper.find('[data-test="component-app"]') // attrib/value 
+  const appComponent = wrapper.find('[data-test="component-app"]'); // attrib/value
   expect(appComponent.length).toEqual(1);
 
   // debug() - Returns an HTML-like string of the wrapper for debugging purposes.
@@ -56,7 +74,4 @@ test('renders without error', () => {
   // toBeTurthy() is the Matcher statement is to clarify what property & how we want to
   // inspect the 'Subject'
   // expect(wrapper).toBeTruthy(); // to ensure return value is true, not falsy values
-
-
 });
-
