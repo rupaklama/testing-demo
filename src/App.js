58166c68332ca757ef4e0bf5c4d9ee7f745a8f0c
Rupak <rupaklamapythondeveloper@gmail.com>
2020-11-01 11:45:43 -0800

    simulate a fake click event to test counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <div data-test="component-app">
-      <h1 data-test="counter-display">The counter is currently: </h1>
-      <button data-test="increment-button">Increment counter</button>
+      <h1 data-test="counter-display">
+        The counter is currently: <span data-test="count">{count}</span>
+      </h1>
+      <button 
+        data-test="increment-button"
+        onClick={() => setCount( count + 1)}
+      >Increment counter 
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -25,25 +25,41 @@ beforeEach(() => {
 // shows button
 test('renders button', () => {
   const button = wrapper.find('[data-test="increment-button"]');
-  expect(button.length).toEqual(1);
+  expect(button.length).toBe(1);
 });
 
 // displays counter
 test('renders counter display', () => {
   const counterDisplay = wrapper.find('[data-test="counter-display"]');
-  expect(counterDisplay.length).toEqual(1);
+  expect(counterDisplay.length).toBe(1);
 });
 
 // starting value
 test('counter starts at 0', () => {
-  const appComponent = wrapper.find('[data-test="component-app"]');
-  expect(appComponent.length).toEqual(1);
+  // text method returns a string of the rendered text of the current render tree
+  // text method to extract the text of an element
+  const count = wrapper.find('[data-test="count"]').text();
+  expect(count).toBe('0');
 });
 
-// click increments counter display
+
+
+// find the display and see if click increments the counter display
 test('clicking on button increments counter display', () => {
-  const appComponent = wrapper.find('[data-test="component-app"]');
-  expect(appComponent.length).toEqual(1);
+  
+  // find the button 
+  const button = wrapper.find('[data-test="increment-button"]');
+
+  // click the button
+  // Simulate a 'click' event - .simulate(event,[mock]) of enzyme
+  // simulate func takes first arg -  html name of the event / normal html DOM event,
+  // Second arg is the mock event object that will be merged with the event object passed
+  // to our event handlers - fake event object
+  button.simulate('click'); // mocking button click
+
+  // test the number has been incremented
+  const count = wrapper.find('[data-test="count"]').text();
+  expect(count).toBe('1')
 });
 
 // test function
@@ -60,10 +76,15 @@ test('renders without error', () => {
   // the selector - .find(selector)
   // although we only care about only one element
 
-  // using data-test-attribute on the top level element of component - 'div' to test rendering
+  // data-test-attribute is an attribute syntax - [type="text"]) 
+  // which is one of the valid CSS enzyme selector to find html elements to render
+  // with Find selector 
+
+  // using data-test-attribute on the component's html elements to be more specific 
+  // using data-test-attribute to easy to find html elements & to render it
   // using expect statement to throw an error, attribute syntax ([href="foo"]
   const appComponent = wrapper.find('[data-test="component-app"]'); // attrib/value
-  expect(appComponent.length).toEqual(1);
+  expect(appComponent.length).toBe(1);
 
   // debug() - Returns an HTML-like string of the wrapper for debugging purposes.
   // helpful if we are not sure what's going on with the test or tests are not passing
