06916e2f83eaf161b06c389c63f2763bdcfd59c0
Rupak <rupaklamapythondeveloper@gmail.com>
2020-11-13 08:50:00 -0800

    created test for GuesssedWords component

--- src/components/GuessedWords.test.js
+++ src/components/GuessedWords.test.js
@@ -12,19 +12,19 @@ import checkPropTypes from 'check-prop-types';
 
 // default props for component to test props type checking
 // every component needs to have default props when doing Props Types Testing
-const defaultProps = 
-  { // Note: DON'T update this default props, if you do find out where else you need to update your code
-    guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }],
-  }
- // NOTE: without this default props, we will get warning errors as
- // each of our test below expects default props which is handled in setup helper func
+const defaultProps = {
+  // Note: DON'T update this default props, if you do, find out where else you need to update your code
+  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }],
+};
+// NOTE: without this default props, we will get warning errors as
+// each of our test below expects default props which is handled in setup helper func
 
 // setup function for dry
-// this func is going to take props, empty object by default
+// this func is going to take props, empty object as arg
 // props is an object with key/value pair
 const setup = (props = {}) => {
   // ...defaultProps - pass in defaultProps
-  // ...props is to add additional props
+  // ...props is to add additional props through props arg
   const setupProps = { ...defaultProps, ...props };
 
   // passing all combine props into our component for testing
@@ -34,18 +34,14 @@ const setup = (props = {}) => {
 // Test stages
 
 test('does not throw warning with expected props', () => {
-
   // NOTE: at first, this test will automatically pass since our component has no props defined
 
   // will see what kind a error we get, if we run checkPropTypes with that our default props
-  // pass .propTypes into our component & props that we want to test,
-  // then tell it we are testing properties 'prop'
-  // then you give name of the component - GuessedWords.name
   const propError = checkPropTypes(
-    GuessedWords.propTypes,
-    defaultProps,
-    'prop',
-    GuessedWords.name
+    GuessedWords.propTypes, // add in .propTypes into our component
+    defaultProps, // our default props
+    'prop', // then tell it we are testing properties 'prop'
+    GuessedWords.name // then you give name of the component - GuessedWords.name
   );
 
   // since we are using checkPropTypes npm package, we get return an error instead of warning
@@ -57,12 +53,55 @@ test('does not throw warning with expected props', () => {
 // describe is just the way of grouping tests context
 // so this help document our tests, somebody looking at our tests
 // can know what we are testing in each sections & also help to document
-// if anyone is looking for failure, it would tell in which one of these context 
+// if anyone is looking for failure, it would tell in which one of these context
 // the failure occurred in
 describe('if there are no words guessed', () => {
+  // data-test-attribute is an attribute syntax - [type="text"])
+  // which is one of the valid CSS enzyme selector to find html elements to render
+  // with Find selector - to find html elements
 
-})
+  // find method/selector returns back an array which contains every elements/instances that matches
+  // the selector - .find(selector)
+  // although we only care about only one element
+
+  // using data-test-attribute on the component's html elements to be more specific
+  // using data-test-attribute to easy to find html elements & to render it
+  // using expect statement to throw an error,
+  // value - attribute syntax ([href="foo"]
+
+  // if there's a common code between multiple tests in a single file,
+  // we use jest's beforeEach helper func to extract that common logic
+  let wrapper;
+  beforeEach(() => {
+    // any logic we put here gets executed before all the tests below
+    // passing new default prop here to test
+    wrapper = setup({ guessedWords: [] });
+  });
+
+  test('renders without error', () => {
+    const component = wrapper.find('[data-test="component-guessed-words"]'); // attrib/value
+    expect(component.length).toBe(1);
+  });
+
+  test('renders instructions to guess a word', () => {
+    const instructions = wrapper.find('[data-test="guess-instructions"]'); 
+    // text length to be non-zero
+    // using not method of jest for that
+    expect(instructions.text().length).not.toBe(0)
+  });
+});
 
 describe('if there are words guessed', () => {
+  
+  test('renders without error', () => {
 
-})
+  })
+
+  test('renders "guessed words" section', () => {
+
+  })
+
+  test('correct number of guessed words', () => {
+    
+  })
+});
