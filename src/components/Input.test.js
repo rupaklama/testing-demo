009a9a33a738e57120145cda2c9ad080e4f55f32
Rupak <rupaklamapythondeveloper@gmail.com>
2020-11-23 09:49:43 -0800

    updated input test file

--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-// helper function with test store
+// helper function with Redux TEST store
 import { storeFactory } from '../../test/testUtils';
 
-// Mounts and renders a react component into the document and provides a testing wrapper around it.
+// shallow method
 import { shallow } from 'enzyme';
 
 import Input from './Input';
 
 // NOTE: This test is for Connected Component - 'Input'
-// which is connected to the Redux Store
-
-// we want to make sure we are using same Reducer as our actual App
+// which is connected to the Redux Store in our actual App
+// Here, we are using our Redux Test Store to test if this
+// component is successfully receiving default state in store
+// like in actual app
 
 // describe is just the way of grouping tests context
 // so this help document our tests, somebody looking at our tests
@@ -20,22 +21,23 @@ import Input from './Input';
 // the failure occurred in
 
 // setup function for dry
-// we need to connect Redux Store inside our setup for testing
+// we need to connect our Redux TEST Store inside our setup for testing
 
 // NOTE: It's important to create fresh Store for each tests for testing
 // & we want to create a TEST STORE for testing that matches configuration of our actual Redux Store
-// we need to connect to Redux Store inside our setup
+// we need to connect to Redux Test Store inside our setup
 // Passing 'store' as a prop to our Connect Component - input
-const setup = (initialState = {}) => { // default initial state of our Test Redux Store
+const setup = (initialState = {}) => {
+  // default initial state of our Test Redux Store
   const store = storeFactory(initialState); // passing default arg from above
 
   // dive meaning to get inside of children components
   // first Enzyme dive method returns/renders react child component of shallow wrapper
-  // in our case, its the Input component wrapped with shallow wrapper below, now
-  // to go one level inside of Input component to access html elements,
+  // in our case, its going to return 'Input 'component wrapped with shallow wrapper below, now
+  // to go one level deeper inside of Input component to access 'html' elements,
   // we need to dive one more time, add one more dive()
 
-  // NOTE: Enzyme suggests that first connect components to Redux store &
+  // NOTE: Enzyme suggests that first connect components to App Redux store &
   // then use dive methods to access its elements
   // This is a better approach to do testing than non-connected components
   // since they are not connected to redux store & they use mock action creators
@@ -51,21 +53,21 @@ const setup = (initialState = {}) => { // default initial state of our Test Redu
 
 // input element renders when 'success' state is false
 describe('render', () => {
-  // if there's a common code between multiple tests in a single file,
-  // we use jest's beforeEach helper func to extract that common logic
-
-  let wrapper;
-
-  beforeEach(() => {
-    // any logic we put here gets executed before all the tests below
-    // initial state for our test contexts here
-    const initialState = { success: false}
-    wrapper = setup(initialState)
-  });
-
   // we are going to have two test contexts
-  // FIRST context 
+  // FIRST context
   describe('word has not been guessed', () => {
+    // success: false
+    // if there's a common code between multiple tests in a single file,
+    // we use jest's beforeEach helper func to extract that common logic
+    let wrapper;
+
+    beforeEach(() => {
+      // any logic we put here gets executed before all the tests below
+      // initial state for our test contexts here
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+
     test('renders component without error', () => {
       // we want to find one div element with this particular data-test-attribute (test class name)
       // to make sure this div element is being render
@@ -74,24 +76,44 @@ describe('render', () => {
     });
 
     test('renders input box', () => {
-      const component = wrapper.find('[data-test="input-box"]');
-      expect(component.length).toBe(1);
+      const inputBox = wrapper.find('[data-test="input-box"]');
+      expect(inputBox.length).toBe(1);
     });
 
     test('renders submit button', () => {
-      const component = wrapper.find('[data-test="submit-button"]');
-      expect(component.length).toBe(1);
+      const submit = wrapper.find('[data-test="submit-button"]');
+      expect(submit.length).toBe(1);
     });
   });
 
   // SECOND test context
   // when initial state is true
   describe('word has been guessed', () => {
-    test('renders component without error', () => {});
+   let wrapper;
 
-    test('does not render input box', () => {});
+    beforeEach(() => {
+      // any logic we put here gets executed before all the tests below
+      // initial state for our test contexts here
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    });
 
-    test('does not render submit button', () => {});
+    test('renders component without error', () => {
+      // we want to find one div element with this particular data-test-attribute (test class name)
+      // to make sure this div element is being render
+      const component = wrapper.find('[data-test="component-input"]');
+      expect(component.length).toBe(1);
+    });
+
+    test('does not render input box', () => {
+      const inputBox = wrapper.find('[data-test="input-box"]')
+      expect(inputBox.length).toBe(0)
+    });
+
+    test('does not render submit button', () => {
+      const submit = wrapper.find('[data-test="submit-button"]');
+      expect(submit.length).toBe(0);
+    });
   });
 });
 
--- test/testUtils.js
+++ test/testUtils.js
@@ -9,7 +9,8 @@ import checkPropTypes from 'check-prop-types';
 // global configurations for all our tests
 // helper function that we can import into all of our test files
 
-// we need to connect Redux Store inside our setup func for testing
+// we need to create Redux Test Store inside our setup func for testing
+// to mock up data flow from redux store to connected components 
 import { createStore } from 'redux';
 
 // NOTE: It's important to create fresh Store for each tests for testing
@@ -19,13 +20,13 @@ import rootReducer from '../src/reducers';
 // we want to make sure we are using same Reducer as our actual App
 // If we add Reducer to our App, it will add it to our 'storeFactory' as well
 // When we add middleware to our store - Redux Thunk, we will share with our 'storeFactory' also
-export const storeFactory = (initialState) => { // param - to specify initial state in our test
+export const storeFactory = (initialState) => { // param - to specify initial state in our test store
   // we want to create a TEST STORE for testing that matches configuration of our actual Redux Store
   // which uses Reducers from our actual App
   return createStore(rootReducer, initialState);
 }
 // NOTE: This 'storeFactory' helper func will be added as a PROP to our connected components
-// which allow connected components to connect to our actual redux store
+// in our test files which allow connected components to connect to our TEST redux store above
 
 // data-test-attribute is an attribute syntax - [type="text"])
 // which is one of the valid CSS enzyme selector to find html elements to render
